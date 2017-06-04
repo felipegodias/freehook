e7c95df70e2a00affd75c5087b6291a6a9b45798
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-04 03:16:18 -0300

    Modified PullerEditor to help with mask creation.

--- UnityProject/Hook/Assets/Scripts/Elements/Editor/PullerEditor.cs
+++ UnityProject/Hook/Assets/Scripts/Elements/Editor/PullerEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(Puller))]
@@ -15,19 +16,38 @@ public class PullerEditor : Editor {
         bool applyMaskButtonClicked = GUILayout.Button("Apply Mask");
         if (applyMaskButtonClicked) {
 
-            
+
+            Transform parent = this.target.transform.parent;
+            Match match = Regex.Match(parent.name, @"(\d+)");
+            string layer = match.Value;
+            int layerId = SortingLayer.NameToID(layer);
             Transform content = this.target.transform.Find("content");
+            Transform pullerEnd = content.transform.Find("puller_end");
+
+            Vector3 direction = pullerEnd.localPosition - content.transform.localPosition;
+            direction = direction.normalized;
+            direction.z = 0;
+
+            float distance = Vector3.Distance(content.transform.localPosition, pullerEnd.localPosition);
+
+            
             SpriteRenderer[] renderers = content.GetComponentsInChildren<SpriteRenderer>();
-            Bounds bounds = renderers[0].bounds;
-            renderers[0].maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-            for (int i = 1; i < renderers.Length; i++) {
+            for (int i = 0; i < renderers.Length; i++) {
                 renderers[i].maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-                bounds.Encapsulate(renderers[i].bounds);
+                renderers[i].sortingLayerID = layerId;
             }
 
             Transform mask = this.target.transform.Find("mask");
-            mask.position = bounds.center;
-            mask.localScale = bounds.size;
+
+            mask.right = direction;
+            mask.localPosition = direction * (distance / 2);
+            mask.localScale = new Vector3(distance, 1, 1);
+
+            SpriteMask spriteMask = mask.GetComponent<SpriteMask>();
+            spriteMask.frontSortingLayerID = layerId;
+            spriteMask.frontSortingOrder = 0;
+            spriteMask.backSortingLayerID = layerId;
+            spriteMask.backSortingOrder = -1;
         }
     }
 
