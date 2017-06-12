179e9d959c15851820bf87f94e090bcbc1430749
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-12 03:08:54 -0300

    Created implementation with google play.

---
+++ UnityProject/Hook/Assets/Scripts/GooglePlay/GooglePlayManager.cs
@@ -0,0 +1,32 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GooglePlayManager : MonoBehaviour {
+
+    private void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start() {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        // requests a server auth code be generated so it can be passed to an
+        //  associated back end server application and exchanged for an OAuth token.
+        .RequestServerAuthCode(false)
+        // requests an ID token be generated.  This OAuth token can be used to
+        //  identify the player to other services such as Firebase.
+        .RequestIdToken()
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+        });
+    }
+
+}
