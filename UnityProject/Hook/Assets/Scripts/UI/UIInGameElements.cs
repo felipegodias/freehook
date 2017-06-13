8748d1bb7fafa043969f1d087decadc7853583fa
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-13 03:43:36 -0300

    Showing in game elements after speedrun.

--- UnityProject/Hook/Assets/Scripts/UI/UIInGameElements.cs
+++ UnityProject/Hook/Assets/Scripts/UI/UIInGameElements.cs
@@ -10,6 +10,7 @@ public class UIInGameElements : MonoBehaviour {
 
     private void Awake() {
         EventManager.AddListener<OnSpeedRunStart>(this.OnSpeedRunStart);
+        EventManager.AddListener<OnSpeedRunEnd>(this.OnSpeedRunEnd);
     }
 
     private void OnSpeedRunStart(object sender, OnSpeedRunStart eventArgs) {
@@ -23,8 +24,20 @@ public class UIInGameElements : MonoBehaviour {
         }, 0, 1, 0.5f).setEaseOutSine();
     }
 
+    private void OnSpeedRunEnd(object sender, OnSpeedRunEnd eventargs) {
+        this.defaultElementsCanvasGroup.interactable = true;
+        this.speedRunElementsCanvasGroup.interactable = false;
+        LeanTween.value(this.gameObject, f => {
+            this.defaultElementsCanvasGroup.alpha = f;
+            this.speedRunElementsCanvasGroup.alpha = 1 - f;
+            this.defaultElementsCanvasGroup.transform.localPosition = Vector3.up * 25 * (1 - f);
+            this.speedRunElementsCanvasGroup.transform.localPosition = Vector3.up * 25 * f;
+        }, 0, 1, 0.5f).setEaseOutSine();
+    }
+
     private void OnDestroy() {
         EventManager.RemoveListener<OnSpeedRunStart>(this.OnSpeedRunStart);
+        EventManager.RemoveListener<OnSpeedRunEnd>(this.OnSpeedRunEnd);
     }
 
 }
