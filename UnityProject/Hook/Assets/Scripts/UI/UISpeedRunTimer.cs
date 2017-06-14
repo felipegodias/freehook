592d03261fe34126ec82690b5bed3f49f686f502
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-14 00:21:12 -0300

    Created extension class for time span.

--- UnityProject/Hook/Assets/Scripts/UI/UISpeedRunTimer.cs
+++ UnityProject/Hook/Assets/Scripts/UI/UISpeedRunTimer.cs
@@ -11,12 +11,7 @@ public class UISpeedRunTimer : MonoBehaviour {
 
     private void Update() {
         TimeSpan timeSpan = this.gameManager.SpeedRunTimeSpan;
-        int hours = timeSpan.Hours;
-        int minutes = timeSpan.Minutes;
-        int seconds = timeSpan.Seconds;
-        int milliseconds = timeSpan.Milliseconds;
-        string text = string.Format("{0:D2}:{1:D2}:{2:D2},{3}", hours, minutes, seconds, milliseconds.ToString("D2").Substring(0, 2));
-        this.text.text = text;
+        this.text.text = timeSpan.ToChronometerString();
     }
 
 }
