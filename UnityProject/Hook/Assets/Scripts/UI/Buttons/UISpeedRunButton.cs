439c6e08c51f11c80ed94bf3fecb11a011537721
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-14 00:07:17 -0300

    Showing the speed run button only if the player finished the game.

--- UnityProject/Hook/Assets/Scripts/UI/Buttons/UISpeedRunButton.cs
+++ UnityProject/Hook/Assets/Scripts/UI/Buttons/UISpeedRunButton.cs
@@ -2,6 +2,13 @@
 
 public class UISpeedRunButton : UIButton {
 
+    private void Awake() {
+        int lastStage = Player.GetLastStage();
+        if (lastStage < 24) {
+            this.gameObject.SetActive(false);
+        }
+    }
+
     protected override void OnClick() {
         EventManager.Dispatch(new OnSpeedRunStart());
     }
