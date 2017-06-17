36566dae5af46d9eeb51e28535a25412cea4fc75
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-17 06:19:36 -0300

    Added support for multi lang in the I18N.

--- UnityProject/Hook/Assets/Scripts/I18N.cs
+++ UnityProject/Hook/Assets/Scripts/I18N.cs
@@ -2,19 +2,25 @@
 
 public class I18N {
 
-    private static readonly Dictionary<string, string> dictionary = new Dictionary<string, string> {
-        {"ads_screen_description", "Watch ads for {0} hearts?"},
-        {"no_more_ads_screen_description", "We don`t have any more ads to show. Please try again later. :("},
-        {"ok", "ok"},
-        {"watch", "watch"},
-        {"remove_ads", "Remove Ads"}
-    };
+    private static readonly Dictionary<string, Dictionary<string, string>> dictionary =
+        new Dictionary<string, Dictionary<string, string>> {
+            {
+                "en", new Dictionary<string, string> {
+                    {"ads_screen_description", "Watch ads for {0} hearts?"},
+                    {"no_more_ads_screen_description", "We don`t have any more ads to show. Please try again later. :("},
+                    {"ok", "ok"},
+                    {"watch", "watch"},
+                    {"remove_ads", "Remove Ads"}
+                }
+            }
+        };
 
     public static string GetText(string key, params object[] args) {
-        if (!dictionary.ContainsKey(key)) {
+        Dictionary<string, string> lang = dictionary["en"];
+        if (!lang.ContainsKey(key)) {
             return null;
         }
-        string text = dictionary[key];
+        string text = lang[key];
         return string.Format(text, args);
     }
 
