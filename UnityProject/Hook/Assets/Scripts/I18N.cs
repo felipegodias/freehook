900ffec77df0155145868476d2109b593ea2b0e1
felipegodias <felipe.g.o.dias@outlook.com>
2017-06-17 15:16:15 -0300

    Using SystemLanguage as key for the I18N dicionary.

--- UnityProject/Hook/Assets/Scripts/I18N.cs
+++ UnityProject/Hook/Assets/Scripts/I18N.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class I18N {
 
-    private static readonly Dictionary<string, Dictionary<string, string>> dictionary =
-        new Dictionary<string, Dictionary<string, string>> {
+    private static readonly Dictionary<SystemLanguage, Dictionary<string, string>> dictionary =
+        new Dictionary<SystemLanguage, Dictionary<string, string>> {
             {
-                "en", new Dictionary<string, string> {
+                SystemLanguage.English, new Dictionary<string, string> {
                     {"ads_screen_description", "Watch ads for {0} hearts?"},
                     {"no_more_ads_screen_description", "We don`t have any more ads to show. Please try again later. :("},
                     {"ok", "ok"},
@@ -14,7 +15,7 @@ public class I18N {
                 }
             },
             {
-                "pt", new Dictionary<string, string> {
+                SystemLanguage.Portuguese, new Dictionary<string, string> {
                     {"ads_screen_description", "Assistir propaganda por {0} corações?"},
                     {"no_more_ads_screen_description", "Desculpe, não temos mais propagandas para mostrar, tente novamente mais tarde. :("},
                     {"ok", "ok"},
@@ -25,7 +26,7 @@ public class I18N {
         };
 
     public static string GetText(string key, params object[] args) {
-        Dictionary<string, string> lang = dictionary["en"];
+        Dictionary<string, string> lang = dictionary[SystemLanguage.English];
         if (!lang.ContainsKey(key)) {
             return null;
         }
