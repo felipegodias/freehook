040fe2dbafb21805b3686044ce24c912397755fb
felipegodias <felipe.g.o.dias@outlook.com>
2018-03-12 20:59:48 -0300

    Added play icon to the first level.

---
+++ UnityProject/Hook/Assets/Scripts/Listeners/TextLightSwitchListener.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+
+using UnityEngine;
+
+public class TextLightSwitchListener : LightSwitchListener
+{
+
+    private TextMeshPro[] texts;
+
+    [SerializeField]
+    private bool useMainColor = true;
+
+    protected override void Awake()
+    {
+        texts = GetComponentsInChildren<TextMeshPro>(true);
+        base.Awake();
+    }
+
+    protected override void UpdateColors()
+    {
+        foreach (TextMeshPro text in texts)
+        {
+            Color oldColor = text.color;
+            Color newColor = useMainColor ? ColorUtils.LineColor : ColorUtils.BackgroundColor;
+            newColor.a = oldColor.a;
+            text.color = newColor;
+        }
+    }
+
+}
