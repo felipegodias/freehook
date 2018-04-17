7d209148debbdd445f32fe405e5101c07a8eba14
felipegodias <felipe.g.o.dias@outlook.com>
2018-04-16 22:51:36 -0300

    Created rate and unlock all achievements.

---
+++ UnityProject/Hook/Assets/Scripts/Achievements/AllUnlockedAchievement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Achievements/Unlock All Achievements")]
+public class AllUnlockedAchievement : Achievement
+{
+
+    public override void Init()
+    {
+    }
+
+}
---
+++ UnityProject/Hook/Assets/Scripts/Achievements/OnRateGameAchievement.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Achievements/Rate Game")]
+public class OnRateGameAchievement : Achievement
+{
+
+    public override void Init()
+    {
+    }
+
+}
